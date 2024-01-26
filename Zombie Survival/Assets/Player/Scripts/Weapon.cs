cc65e0e9fedc4d42fe3def69ad71c7f26ab2e464
Nadeau15 <felix.nadeau15@hotmail.com>
2024-01-26 14:24:20 -0500

    up to 164

--- Zombie Survival/Assets/Player/Scripts/Weapon.cs
+++ Zombie Survival/Assets/Player/Scripts/Weapon.cs
@@ -10,6 +10,12 @@ public class Weapon : MonoBehaviour
   [SerializeField] float weaponDamage = 25f;
   [SerializeField] float headshotDamageMultiplier = 2f;
   [SerializeField] ParticleSystem muzzleFlash;
+  [SerializeField] GameObject woodHitParticleFX;
+  [SerializeField] GameObject metalHitParticleFX;
+  [SerializeField] GameObject fleshHitParticleFX;
+  [SerializeField] GameObject sandHitParticleFX;
+  [SerializeField] GameObject stoneHitParticleFX;
+
 
   void Update()
   {
@@ -23,18 +29,19 @@ public class Weapon : MonoBehaviour
     ProcessRaycast();
   }
 
-    void PlayMuzzleFlash() {
-      muzzleFlash.Play();
-    }
+  void PlayMuzzleFlash() {
+    muzzleFlash.Play();
+  }
 
-    void ProcessRaycast()
-  {
+  void ProcessRaycast() {
     RaycastHit hit;
 
     if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
     {
-      float damageDealth = weaponDamage;
-      // TODO: add hit effects
+      float damageDealth = weaponDamage;    
+
+      ProcessHitParticles(hit);  
+
       EnemyHealth targetHealth = hit.transform.GetComponentInParent<EnemyHealth>();
       if (targetHealth == null) {
         return;
@@ -46,5 +53,30 @@ public class Weapon : MonoBehaviour
 
       targetHealth.TakeDamage(damageDealth);
     }
+  }
+
+  void ProcessHitParticles(RaycastHit targetHit) {
+    Material targetMaterial = targetHit.transform.GetComponent<Renderer>().material;
+    GameObject impact = new GameObject();
+
+    switch(targetMaterial.name) {
+      case "WoodSurface":
+        impact = Instantiate(woodHitParticleFX, targetHit.point, Quaternion.LookRotation(targetHit.normal));
+        break;
+      case "MetalSurface":
+        impact = Instantiate(metalHitParticleFX, targetHit.point, Quaternion.LookRotation(targetHit.normal));
+        break;
+      case "SandSurface":
+        impact = Instantiate(sandHitParticleFX, targetHit.point, Quaternion.LookRotation(targetHit.normal));
+        break;
+      case "FleshSurface":
+        impact = Instantiate(fleshHitParticleFX, targetHit.point, Quaternion.LookRotation(targetHit.normal));
+        break;
+      case "StoneSurface":
+        impact = Instantiate(stoneHitParticleFX, targetHit.point, Quaternion.LookRotation(targetHit.normal));
+        break;
+    }
+
+    Destroy(impact, 1);
   }
 }
